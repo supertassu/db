4544bbd1de7e71ddc4ae93c697eb9aa274caf7c1
Tassu <git@tassu.me>
2018-04-13 17:25:03 +0300

    fix imports on example (whoops)
    Signed-off-by: Tassu <git@tassu.me>

--- example/src/main/java/me/tassu/db/example/DBExample.java
+++ example/src/main/java/me/tassu/db/example/DBExample.java
@@ -1,9 +1,7 @@
 package me.tassu.db.example;
 
-import me.tassu.db.sql.column.Column;
 import me.tassu.db.impl.sql.mysql.MySQLDatabase;
-import me.tassu.db.sql.DataType;
-import me.tassu.db.sql.Table;
+import me.tassu.db.sql.column.Column;
 import me.tassu.db.sql.table.Table;
 import me.tassu.db.sql.util.DataType;
 
